48898dc875c1722f8b379dd952ee947d6be95358
Edi1201 <77607534+Edi1201@users.noreply.github.com>
2021-02-23 09:27:24 +0100

    zepsulo sie i nie dziala

--- 1.9 tablice i pliki/Tablice.java
+++ 1.9 tablice i pliki/Tablice.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
 import java.util.Random;
 import java.io.*;
-public class Tablice{
+class Tablice{
 	public static void main(String args[]){
         Random r = new Random();
-        int tab[] = new int[10]; 
-        //Wypełnianie tablicy danymi -> pętla FOR
-        for (int i=0;i<tab.length;i++){
-            tab[i] = r.nextInt(6)+1;
-            System.out.println(tab[i]);
+        int grades[] = new int[10];
+        for (int i=0;i<grades.length;i++){
+            grades[i] = r.nextInt(6)+1;
+            System.out.println(grades[i]);
         }
-    }
+        String students[] = new String[10];
+
+        File f = new File("uczniowie.txt");
+        try{
+            Scanner sc = new Scanner(f);
+            while(sc.hasNext()){
+                int i=0;
+
+                students[i] = sc.nextLine()+"\n";
+                System.out.println(students[i]);
+                i++;
+            }
+        }catch(FileNotFoundException ex) {
+            System.out.println(ex.toString());
+        }
+    }  
 }
