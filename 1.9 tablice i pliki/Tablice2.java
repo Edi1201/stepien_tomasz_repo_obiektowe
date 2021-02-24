69b1c61c38557093eec716631b677e71c06a2372
Edi1201 <77607534+Edi1201@users.noreply.github.com>
2021-02-24 10:24:16 +0100

    Stacja V: Nowa wersja Javy pomaga wstać Javie

--- 1.9 tablice i pliki/Tablice2.java
+++ 1.9 tablice i pliki/Tablice2.java
@@ -3,22 +3,17 @@ import java.util.Random;
 import java.io.*;
 class Tablice2{
     public static void main(String args[]){
-        Random r = new Random();
-        File f = new File("czylosowac.txt");
-        String czyLosowac[] = new String[10];
         try{
+            Random r = new Random();
+            File f = new File("czylosowac.txt");
             Scanner sc = new Scanner(f);
             int i=0;
+            String tekst = sc.nextLine();
+            System.out.println(tekst);
+            String czyLosowac[] = tekst.split(" ");
             for(i=0;i<10;i++){
-                String podzielone[] = sc.next().split(" ");
-                czyLosowac[i] = podzielone[i];
                 System.out.println(czyLosowac[i]);
             }
-            /*String studentsGrades[] = new String[10];
-            for (i=0;i<studentsGrades.length;i++){
-                studentsGrades[i] = students[i]+" "+grades[i];
-                System.out.println(studentsGrades[i]);
-            }*/
         }catch(FileNotFoundException ex) {
             System.out.println(ex.toString());
         }
