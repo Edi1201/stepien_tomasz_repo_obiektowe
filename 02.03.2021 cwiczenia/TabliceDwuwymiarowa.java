8e3be0f7b76e2283aad7be6d5e262534bc1d39e3
Edi1201 <77607534+Edi1201@users.noreply.github.com>
2021-03-02 10:31:20 +0100

    Wiadomo z internetu zadanie bo sam nie umiem

--- 02.03.2021 cwiczenia/TabliceDwuwymiarowa.java
+++ 02.03.2021 cwiczenia/TabliceDwuwymiarowa.java
@@ -1,9 +1,10 @@
 class TabliceDwuwymiarowa{
     public static void main(String [] args){
-        tablica2x2();
+        //tablica2x2();
+        tablicaMnozenia();
     }
-    private static void tablica2x2(){
-        int  tab[][] = new int[2][2];
+    /*private static void tablica2x2(){
+        int  tab [][] = new int[2][2];
         tab[0][0] = 1;
         tab[0][1] = 2;
         tab[1][0] = 3;
@@ -12,6 +13,20 @@ class TabliceDwuwymiarowa{
             for(int j=0;j<tab[i].length;j++){
                 System.out.println(tab[i][j]);
             }
+        }
+    }*/
+    private static void tablicaMnozenia(){
+        int  tab [][] = new int[10][10];
+        for (int i=0;i<tab.length;i++){
+            for(int j=0;j<tab[i].length;j++){
+                tab [i][j] = (i+1)*(j+1);
+            }
+        }
+        for (int i=0;i<tab.length;i++){
+            for(int j=0;j<tab[i].length;j++){
+                System.out.print(tab[i][j]+"\t");
+            }
+            System.out.println();
         }
     }
 }
