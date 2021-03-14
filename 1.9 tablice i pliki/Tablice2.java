2dc71c62bc3338951cccd2ee25be6348033266e3
Edi1201 <77607534+Edi1201@users.noreply.github.com>
2021-03-14 21:29:55 +0100

    zrobione zadania

--- 1.9 tablice i pliki/Tablice2.java
+++ 1.9 tablice i pliki/Tablice2.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 import java.io.*;
 class Tablice2{
     public static void main(String args[]){
         try{
             Random r = new Random();
             File f = new File("czylosowac.txt");
+            File f2 = new File("ileliczb.txt");
             Scanner sc = new Scanner(f);
-            int i=0;
+            Scanner sc2 = new Scanner(f2);
             String tekst = sc.nextLine();
-            System.out.println(tekst);
             String czyLosowac[] = tekst.split(" ");
-            for(i=0;i<10;i++){
-                System.out.println(czyLosowac[i]);
+            String ile[] = new String[czyLosowac.length];
+            int i=0;
+            int j=0;
+            while(sc2.hasNext()){
+                ile[i] = sc2.nextLine();
+                i++;
+            }
+            for(i=0;i<czyLosowac.length;i++){
+                if(czyLosowac[i].equals("0")){
+                    System.out.println("Wczytalem 0 nie losuje");
+                }else{
+                    for(j=0;j<Integer.parseInt(ile[i]);j++){
+                        System.out.print(r.nextInt(79)+1+" ");
+                    }
+                    System.out.print("\n");
+                }
             }
         }catch(FileNotFoundException ex) {
             System.out.println(ex.toString());
