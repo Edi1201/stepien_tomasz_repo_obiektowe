2d222f75fc1e89c78e425d2a34ec3297031e457e
Edi1201 <77607534+Edi1201@users.noreply.github.com>
2021-03-30 09:07:19 +0200

    1 Bezsensowny commit :)

---
+++ 1.9.1 Tworzenie obiektow/CwiczenieObiektowe_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class CwiczenieObiektowe_1{
+        static MojeDane md;
+    	public static void main (String[] args){
+        md = new MojeDane();
+        System.out.println(md.imie);
+        System.out.println(md.nazwisko);
+        System.out.println(md.klasa);
+	}
+}
---
+++ 1.9.1 Tworzenie obiektow/MojeDane.java
@@ -0,0 +1,13 @@
+class MojeDane{
+	public String imie = "Tomasz";
+	public String nazwisko = "Stepien";
+    public String klasa = "2PTN";
+		
+	//KONSTRUKTOR
+	public MojeDane(){
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+        this.klasa = klasa;
+	}
+
+}
