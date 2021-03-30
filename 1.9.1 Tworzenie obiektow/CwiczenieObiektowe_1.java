69a97fdff136b72096c3a7952a64529d3aa870ed
Edi1201 <77607534+Edi1201@users.noreply.github.com>
2021-03-30 09:33:06 +0200

    elegancja francja

--- 1.9.1 Tworzenie obiektow/CwiczenieObiektowe_1.java
+++ 1.9.1 Tworzenie obiektow/CwiczenieObiektowe_1.java
@@ -8,7 +8,10 @@ class CwiczenieObiektowe_1{
         System.out.println(md.imie);
         System.out.println(md.nazwisko);
         System.out.println(md.klasa);
+        System.out.println();
         log = new Logowanie();
+        System.out.println(log.toString());
+        System.out.println();
         k1 = new Kola(4);
         System.out.println("Pole i obwod kola 1:");
         System.out.println(k1.pole_kola());
